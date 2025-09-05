84e16867a74138b3b6b3720009ab01ad6231e5a0
subhajitlucky <subhajitpradhan310@gmail.com>
2025-09-05 16:23:54 +0000

    feat: implement basic Calculator class with add and subtract methods

---
+++ day_01/calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def __init__(self):
+        print("Calculator ready!")
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
